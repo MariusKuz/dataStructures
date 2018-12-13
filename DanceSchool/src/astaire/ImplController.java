4193dbcba69d268e4f614fea8125fc00309942a1
Marius Kuzminskas <berniukas.marius@gmail.com>
2018-12-13 13:09:39 +0000

    123

--- DanceSchool/src/astaire/ImplController.java
+++ DanceSchool/src/astaire/ImplController.java
@@ -7,20 +7,22 @@ import java.io.IOException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.Arrays;
 
 public class ImplController implements Controller {
 
+	int count = 0;
+	int count2 = 0;
+
 	@Override
 	public String listAllDancersIn(String dance) {
 		// get CSV file for dances Data
-	
+		ArrayList<String> dancesData = getCSV("src/csvFiles/danceShowData_dances.csv");
 
-		Set<String> dancesData = new TreeSet<>(getCSV("src/csvFiles/danceShowData_dances.csv"));
 		String result = "";
 
 		// for each line in dances csv file
@@ -50,16 +52,15 @@ public class ImplController implements Controller {
 
 		return result;
 	}
-	
+
 	public String[] splitByComma(String names) {
-		return names.split(",");
+		return names.split(", ");
 	}
 
 	public String getDanceGroupMembers(String name) {
 		// get dance group data
+		ArrayList<String> danceGroupsData = getCSV("src/csvFiles/danceShowData_danceGroups.csv");
 
-		Set<String> danceGroupsData = new TreeSet<>(getCSV("src/csvFiles/danceShowData_danceGroups.csv"));
-		
 		// result by default is just name of given
 		String result = name;
 
@@ -68,78 +69,166 @@ public class ImplController implements Controller {
 			String[] splitByTab = line.split("\t");
 
 			// if, at any point, name of dance group is equal to given name
-			if (splitByTab[0].equals(name) ) {
-				// return names of dancers in group 
+			if (splitByTab[0].equals(name)) {
+				// return names of dancers in group
 				result = getDanceGroupMembers(splitByTab[1]);
 			}
 		}
 
 		return result;
 	}
-	
+
 	@Override
-	public String listAllDancesAndPerformers() {
-		
+	public String listAllDancesAndPerformers(String filename) {
+
 		// get CSV file for dances Data
-		TreeSet<String> dancesData = getCSV("src/csvFiles/danceShowData_dances.csv");
+		ArrayList<String> dancesData = getCSV("src/csvFiles/" + filename);
 
 		int lineNumber = 0;
 		String result = "";
-		
-		//for each line in dances csv file
+
+		// for each line in dances csv file
 		for (String line : dancesData) {
-			
-			//split into two sections - [0] is name of dance & [1] is dancers
+
+			// split into two sections - [0] is name of dance & [1] is dancers
 			String[] splitByTab = line.split("\t");
-			
-			
 
-			
-			lineNumber++; //print line number
-			result += lineNumber + ": "; 
-			result += (splitByTab[0].trim()) + "\n"; //print group name
-			
-			
-			
-		
-			String finalResult =(listAllDancersIn(splitByTab[0].trim())) ; //find out all the dancers in the dance
-			String[] elements = finalResult.split(","); // split them by commas and place them into array
-			
-			for(int i = 0; i < elements.length; i++) {
-				elements[i] = elements[i].trim(); //trim every element in the array
-			}
-			
-			
-			List<String> fixedLenghtList = Arrays.asList(elements); //put the array elements into a list
-			ArrayList<String> listOfString = new ArrayList<String>(fixedLenghtList); //put the elements of the list into an arrayList
-			Collections.sort(listOfString); //sort the array list alphabetically
-			result += listOfString.toString().replace("[","").replace("]",""); //remove brackets from the answers and add to result
-			result += "\n"; 
-
+			lineNumber++;
+			result += lineNumber + ": ";
+			result += (splitByTab[0].trim()) + "\n";
+			result += (listAllDancersIn(splitByTab[0].trim())) + "\n";
 		}
-		
+
 		return result;
 	}
-	 
 
 	@Override
 	public String checkFeasibilityOfRunningOrder(String filename, int gaps) {
-		// TODO Auto-generated method stub
-		return null;
+
+		// get CSV file for dances Data
+		ArrayList<String> runningOrder = getCSV("src/csvFiles/" + filename);
+
+		System.out.println(listAllDancesAndPerformers(filename));
+
+		return checkRunningOrder(runningOrder, gaps);
+	}
+
+	private String checkRunningOrder(ArrayList<String> runningOrder, int gaps) {
+
+		// For each line (dance) in running order
+		for (int i = 0; i < runningOrder.size() - gaps; i++) {
+
+			// get individual names
+			String[] splitByTab = runningOrder.get(i).split("\t");
+			String[] dancers = listAllDancersIn(splitByTab[0].trim()).split(", ");
+
+			String[] nextDancers;
+
+			// from current dance to current dance PLUS given gaps
+			for (int a = i + 1; a < i + gaps; a++) {
+
+				// get individual names
+				splitByTab = runningOrder.get(a).split("\t");
+				nextDancers = listAllDancersIn(splitByTab[0].trim()).split(", ");
+
+				// for each dancer in current dance
+				for (String dancer : dancers) {
+
+					// for each dancer in next dances - up until current dance + gaps
+
+					for (String nextDancer : nextDancers) {
+
+						// if any dancers repeated
+						if (dancer.equals(nextDancer)) {
+							return "This running order is not feasible. " + dancer + " needs to change in " + (a - i)
+									+ " dances, \n between dance number " + (i + 1) + " and dance number " + (a + 1);
+						}
+					}
+				}
+			}
+		}
+
+		return "This running order is feasible.";
 	}
 
 	@Override
 	public String generateRunningOrder(int gaps) {
-		// TODO Auto-generated method stub
-		return null;
+		// get CSV file for dances Data
+		ArrayList<String> dancesData = getCSV("src/csvFiles/danceShowData_dances.csv");
+
+		return checkRunningOrder(gaps, dancesData);
 	}
 
-	public TreeSet<String> getCSV(String file) {
+	private String checkRunningOrder(int gaps, ArrayList<String> runningOrder) {
+
+		Random ran  = new Random();
+
+		String result = "";
+
+		// For each line (dance) in running order
+		for (int i = count2; i < runningOrder.size() - gaps; i++) {
+
+			// get individual names
+			String[] splitByTab = runningOrder.get(i).split("\t");
+			String[] dancers = listAllDancersIn(splitByTab[0].trim()).split(", ");
+
+			String[] nextDancers;
+
+			// from current dance to current dance PLUS given gaps
+			for (int a = i + 1; a < i + gaps; a++) {
+
+				// get individual names
+				splitByTab = runningOrder.get(a).split("\t");
+				nextDancers = listAllDancersIn(splitByTab[0].trim()).split(", ");
+
+				// for each dancer in current dance
+				for (String dancer : dancers) {
+
+					// for each dancer in next dances - up until current dance + gaps
+
+					for (String nextDancer : nextDancers) {
+						// if any dancers repeated
+						if (dancer.equals(nextDancer)) {
+//							needsSwap = true;
+//							toSwap = a;
+//							String temp = runningOrder.get(a+1);
+//							runningOrder.add(a+1, runningOrder.get(a));
+//							runningOrder.add(a, temp);
+
+							System.out.println("Swapped: " + runningOrder.get(a).split("\t")[0] + " with: " + runningOrder.get(a+1).split("\t")[0]);
+							Collections.swap(runningOrder, a, ran.nextInt((runningOrder.size()-a))+a);
+							
+//							System.out.println("\n\n Current running Order: \n");
+//							for (String line : runningOrder) {
+//								System.out.println(line);
+//							}
+							
+							if (count > 1000) {
+								return "\nA feasible running order could not be found";
+							}
+							count++;
+							
+							checkRunningOrder(gaps, runningOrder);
+						}
+					}
+				}
+			}
+		}
+
+		result += "The following running order will work: \n";
+		for (String line : runningOrder) {
+			result += line + "\n";
+		}
+
+		return result;
+	}
+
+	public ArrayList<String> getCSV(String file) {
 
 		// Print program menu and loop till the user exit
 
 		BufferedReader buffer = null;
-		TreeSet<String> data = new TreeSet<>();
+		ArrayList<String> data = new ArrayList<>();
 		try {
 			String line;
 			buffer = new BufferedReader(new FileReader(file));
